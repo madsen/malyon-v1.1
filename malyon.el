b15eb6e91fda70aa52973bc7073bc7700b96352b
Peter Ilberg <peter.ilberg@gmail.com>
2011-08-22 17:56:37 +0000

    Add MIT license

--- malyon.el
+++ malyon.el
@@ -1,10 +1,29 @@
 ; malyon.el --- mode to execute z code files version 3, 5, 8
 
-;; Copyright (C) 1999-2009 Peter Ilberg
-
 ;; Maintainer: Peter Ilberg <peter.ilberg@gmail.com>
 
-;; Credits:
+;; Copyright (C) 1999-2011 Peter Ilberg
+
+;; Permission is hereby granted, free of charge, to any person obtaining a
+;; copy of this software and associated documentation files (the "Software"),
+;; to deal in the Software without restriction, including without limitation
+;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;; and/or sell copies of the Software, and to permit persons to whom the
+;; Software is furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;; DEALINGS IN THE SOFTWARE.
+
+;;; Credits:
+
 ;;   The author would like to thank the following people for reporting
 ;;   bugs, testing, suggesting and/or contributing improvements:
 ;;     Bernhard Barde, Jonathan Craven, Alberto Petrofsky, Alan Shutko
